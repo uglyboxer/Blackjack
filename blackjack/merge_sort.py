b241a139c289b43d44dc1ca7a0cf286afdef08a1
Cole Howard <uglyboxer@gmail.com>
2015-10-20 20:59:30 -0700

    Update merge test

--- blackjack/merge_sort.py
+++ blackjack/merge_sort.py
@@ -23,5 +23,11 @@ def merge_sort(lst):
 
 if __name__ == '__main__':
 	a = [5, 3, 1234, 2, 5, 1, 6, 9, 23]
-	assert merge_sort(a) == [1, 2, 3, 5, 5, 6, 9, 23, 1234]
+	assert merge_sort(a) == [1, 2, 3, 5, 5, 6, 9, 23, 1234] # Odd # of elems
+	b = [1, 2, 3, 4]
+	c = [6, 7, 8, 1, 2, 3]
+	d = [5, 5, 5, 5]
+	assert merge_sort(b) == [1, 2, 3, 4]  # Pre-sorted
+	assert merge_sort(c) == [1, 2, 3, 6, 7, 8] # Even #
+	assert merge_sort(d) == [5, 5, 5, 5]  # All identical elems
 	print("victory")
